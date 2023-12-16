12e24792471fa92b0a255d8f898b3379719b3aee
Henry <henry.bogita@student.moringaschool.com>
2023-12-16 16:54:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// global scope
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+ // modify global customerName
+ window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+ // set global bestCustomer
+ window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+ window.bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'person I dislike';
+
+function changeLeastFavoriteCustomer(newLeastFavorite) {
+ leastFavoriteCustomer = newLeastFavorite;
+}
